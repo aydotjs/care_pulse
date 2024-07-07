22f746e84f4b941439f98d6a57dda45895511df0
aydotjs <ayobami66@gmail.com>
2024-07-07 10:48:05 +0100

    setting up home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Butcherman } from "next/font/google";
 import Image from "next/image";
 
 export default function Home() {
   return (
   <div>
     <h1 className="text-white text-3xl underline">Care Pulse</h1>
+    <Button>Click Me</Button>
   </div>
   );
 }
